846dec85a2c3e086a2877a50f583f20a5d1aa93a
hpdhillon <hd227@scarletmail.rutgers.edu>
2025-01-16 18:07:28 -0500

    updated code, revised locally. works now.

--- TinderForFood.py
+++ TinderForFood.py
@@ -2,79 +2,104 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Tinder for Food! Start Swiping!')
 
 from openai import OpenAI
 import os
 
 
-openai_key = st.secrets["openai"]
+openai_key = st.secrets['openai_key']
 
 client = OpenAI(api_key = openai_key)
 
-st.session_state.responses_to_avoid = ''
-if st.session_state.responses_to_avoid == '':
-    chatgpt_response = ''
+try:
+        
+    st.session_state.responses_to_avoid
 
-sentence = "im hungry"
+    #st.title('Tinder for Food! Start Swiping!')
 
 
-messages = [
-    {"role": "system", "content": "You are an autonomous agent."},
-    {
-        "role": "user",
-        "content": (
-            '''Based on the user input, give a response that 
-            indicates which food they should eat. \n
-            1. Slice of Pizza: Response: slice-of-pizza\n
-            2. Cheeseburger: Response: cheeseburger\n
-            3. French Fries, Response: french-fries\n
-            4. Grilled Chicken Sandwich, Response: grilled-chicken-sandwich\n
-            5. BBQ Ribs, Response: bbq-ribs\n
-            6. Hot Dog, Response: hot-dog\n
-            7. Steak, Response: steak\n
-            '''
-            + f"\n\nONLY ANSWER WITH ONE OF THE RESPONSES ABOVE, NOT A WORD LESS OR MORE.\n\nUser Input: {sentence}"
+    swiping_empty_mechanism = st.empty()
+    if st.session_state.counter == 1:
+        swiping_empty_mechanism.image(f'{st.session_state.responses_to_avoid}.jpg')
+
+
+
+    def reload():
+        swiping_empty_mechanism.empty()
+
+        st.session_state.counter = 2
+        st.session_state.responses_to_avoid = open('responses.txt').read()
+
+
+        messages = [
+            {"role": "system", "content": "You are an autonomous agent."},
+            {
+                "role": "user",
+                "content": (
+                    '''Based on the user input, give a response that 
+                    indicates which food they should eat. \n
+                    1. Slice of Pizza: Response: slice-of-pizza\n
+                    2. Cheeseburger: Response: cheeseburger\n
+                    3. French Fries, Response: french-fries\n
+                    4. Grilled Chicken Sandwich, Response: grilled-chicken-sandwich\n
+                    5. BBQ Ribs, Response: bbq-ribs\n
+                    6. Hot Dog, Response: hot-dog\n
+                    7. Steak, Response: steak\n
+                    '''
+                    + f"\n\nONLY ANSWER WITH ONE OF THE RESPONSES ABOVE, NOT A WORD LESS OR MORE. EX: Slice of Pizza = slice-of-pizza\n\nUser Input: {st.session_state.sentence}, don't recommend any of: {st.session_state.responses_to_avoid}"
+                )
+            }
+        ]
+
+
+
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            max_tokens=40  # Expecting a filename as output
         )
-    }
-]
 
-response = client.chat.completions.create(
-    model="gpt-4o",
-    messages=messages,
-    max_tokens=40  # Expecting a single number as output
-)
+        chatgpt_response = response.choices[0].message.content
 
-st.write(chatgpt_response)
+        st.image(f'{chatgpt_response}.jpg')
 
-if chatgpt_response=='':
+        chatgpt_response = ', ' + chatgpt_response
+
+        with open('responses.txt', 'a') as file:
+            file.write(chatgpt_response)
+
+    if 'button' not in st.session_state:
+        st.session_state.button = False
+
+    def click_button():
+        st.balloons()
+        st.session_state.button = not st.session_state.button
+        st.session_state.pop('responses_to_avoid')
+
+    columns = st.columns([1, 8, 1])
+    with columns[0]:
+        st.button('❌', on_click=reload)
+    with columns[2]:
+        st.button('✅', on_click=click_button)
+
+   
+except:
+
+    st.title('Tinder for Food! Start Swiping!')
+
     empty_mechanism = st.empty()
 
-    chatgpt_response = response.choices[0].message.content
+    sentence  = st.text_area("What're you in the mood for?")
 
-    chatgpt_response = chatgpt_response.replace('Response: ', '')
+    st.session_state.sentence = sentence
 
-    empty_mechanism.image(f'{chatgpt_response}.jpg')
-
-    # Clear the text file and add the chatgpt_response to it
-    with open('responses.txt', 'w') as file:
-        file.truncate(0)
-        file.write(chatgpt_response)
-
-    st.write("stuck_here")
-
-def reload():
-    empty_mechanism.empty()
-
-    st.session_state.responses_to_avoid = open('responses.txt').read()
-
-    messages = [
+    if st.button('Submit'):
+        messages = [
         {"role": "system", "content": "You are an autonomous agent."},
         {
             "role": "user",
             "content": (
-                '''Based on the user input, give a response that 
-                indicates which food they should eat. \n
+                '''Based on the user input, provide a response of food recommendation. \n
                 1. Slice of Pizza: Response: slice-of-pizza\n
                 2. Cheeseburger: Response: cheeseburger\n
                 3. French Fries, Response: french-fries\n
@@ -83,42 +108,31 @@ def reload():
                 6. Hot Dog, Response: hot-dog\n
                 7. Steak, Response: steak\n
                 '''
-                + f"\n\nONLY ANSWER WITH ONE OF THE RESPONSES ABOVE, NOT A WORD LESS OR MORE. EX: Slice of Pizza = slice-of-pizza\n\nUser Input: {sentence}, don't recommend any of: {st.session_state.responses_to_avoid}, take that into account"
+                + f"\n\nONLY ANSWER WITH ONE OF THE RESPONSES ABOVE, NOT A WORD LESS OR MORE.\n\nUser Input: {sentence}"
             )
         }
-    ]
+        ]
 
-    st.write(f"\n\nONLY ANSWER WITH ONE OF THE RESPONSES ABOVE, NOT A WORD LESS OR MORE. EX: Slice of Pizza = slice-of-pizza\n\nUser Input: {sentence}, don't recommend any of: {st.session_state.responses_to_avoid}, take that into account")
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            max_tokens=40  # Expecting a single number as output
+        )
 
+        chatgpt_response = response.choices[0].message.content
 
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        max_tokens=40  # Expecting a filename as output
-    )
+        chatgpt_response = chatgpt_response.replace('Response: ', '')
 
-    chatgpt_response = response.choices[0].message.content
+        #empty_mechanism.image(f'{chatgpt_response}.jpg')
 
-    st.write(chatgpt_response)
+        # Clear the text file and add the chatgpt_response to it
+        with open('responses.txt', 'w') as file:
+            file.truncate(0)
+            file.write(chatgpt_response)
 
-    st.image(f'{chatgpt_response}.jpg')
+        st.session_state.responses_to_avoid = open('responses.txt').read()
+        if sentence:
+            empty_mechanism.empty()
+            st.session_state.counter = 1
+            st.button('start swiping!')
 
-    chatgpt_response = ', ' + chatgpt_response
-
-    with open('responses.txt', 'w') as file:
-        file.write(chatgpt_response)
-
-    st.write(st.session_state.responses_to_avoid)
-if 'button' not in st.session_state:
-    st.session_state.button = False
-
-def click_button():
-    st.session_state.button = not st.session_state.button
-
-
-while not st.session_state.button:
-
-    st.columns(2)
-    st.button('✅', on_click=click_button)
-
-    st.button('❌', on_click=reload)

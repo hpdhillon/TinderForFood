4bfda628cd7170864b6860cc3f12945fd078a330
hpdhillon <hd227@scarletmail.rutgers.edu>
2025-01-16 01:09:16 -0500

    update

--- TinderForFood.py
+++ TinderForFood.py
@@ -14,10 +14,44 @@ client = OpenAI(api_key = openai_key)
 
 # Get user input
 sentence = st.text_area("what are you hungry for?")
+# Create the messages based on the jpg files
+messages = [
+    {"role": "system", "content": "You are an autonomous agent."},
+    {
+        "role": "user",
+        "content": (
+            '''Based on the user input, give a response that 
+            indicates which food they should eat. \n
+            1. Slice of Pizza: Response: slice-of-pizza\n
+            2. Burger: Response: burger\n
+            3. French Fries, Response: french-fries\n
+            4. Grilled Chicken Sandwich, Response: grilled-chicken-sandwich\n
+            5. BBQ Ribs, Response: bbq-ribs\n
+            6. Hot Dog, Response: hot-dog\n
+            7. Steak, Response: steak\n
+            '''
+            + f"\n\nONLY ANSWER WITH THE RESPONSES ABOVE, NOT A WORD LESS OR MORE.\n\nUser Input: {sentence}"
+        )
+    }
+]
 
-if st.button("Submit"):
-    # Create the messages based on the jpg files
-    messages = [
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=messages,
+    max_tokens=40  # Expecting a single number as output
+)
+st.image(f'{response.choices[0].message.content}.jpg')
+columns = st.columns(3)
+with columns[0]:
+    st.button(
+        "❌")
+with columns[2]:
+    st.button(
+        "✅")
+
+if st.button("❌"):
+    while not st.button( "✅"):
+        messages = [
         {"role": "system", "content": "You are an autonomous agent."},
         {
             "role": "user",
@@ -35,54 +69,18 @@ if st.button("Submit"):
                 + f"\n\nONLY ANSWER WITH THE RESPONSES ABOVE, NOT A WORD LESS OR MORE.\n\nUser Input: {sentence}"
             )
         }
-    ]
+        ]
 
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        max_tokens=40  # Expecting a single number as output
-    )
-    st.image(f'{response.choices[0].message.content}.jpg')
-    columns = st.columns(3)
-    with columns[0]:
-        st.button(
-            "❌")
-    with columns[2]:
-        st.button(
-            "✅")
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages=messages,
+            max_tokens=40  # Expecting a single number as output
+        )
 
-    if st.button("❌"):
-        while not st.button( "✅"):
-            messages = [
-            {"role": "system", "content": "You are an autonomous agent."},
-            {
-                "role": "user",
-                "content": (
-                    '''Based on the user input, give a response that 
-                    indicates which food they should eat. \n
-                    1. Slice of Pizza: Response: slice-of-pizza\n
-                    2. Burger: Response: burger\n
-                    3. French Fries, Response: french-fries\n
-                    4. Grilled Chicken Sandwich, Response: grilled-chicken-sandwich\n
-                    5. BBQ Ribs, Response: bbq-ribs\n
-                    6. Hot Dog, Response: hot-dog\n
-                    7. Steak, Response: steak\n
-                    '''
-                    + f"\n\nONLY ANSWER WITH THE RESPONSES ABOVE, NOT A WORD LESS OR MORE.\n\nUser Input: {sentence}"
-                )
-            }
-            ]
-
-            response = client.chat.completions.create(
-                model="gpt-4",
-                messages=messages,
-                max_tokens=40  # Expecting a single number as output
-            )
-
-            columns = st.columns(3)
-            with columns[0]:
-                st.button(
-                    "❌")
-            with columns[2]:
-                st.button(
-                    "✅")
+        columns = st.columns(3)
+        with columns[0]:
+            st.button(
+                "❌")
+        with columns[2]:
+            st.button(
+                "✅")
